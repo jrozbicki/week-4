9900d11412aca4779414c9891bf0f2e8b4e18ef8
jrozbicki <j.rozbicki9@gmail.com>
2018-08-17 14:55:06 +0200

    Adding task is now available

---
+++ index.js
@@ -0,0 +1,41 @@
+let counter = 0;
+
+document.querySelector(".btn-large").addEventListener("click", () => {
+    if(counter<7){
+    const newLi = document.createElement("li");
+    newLi.setAttribute("class", "single-task");
+
+    const newInput = document.createElement("input");
+    newInput.setAttribute("type", "text");
+    newInput.setAttribute("placeholder", "Tytuł zadania...");
+    newLi.appendChild(newInput);
+
+    const newA1 = document.createElement("a");
+    newA1.setAttribute("class", "waves-effect waves-light btn-small completeBtn");
+
+    const newI1 = document.createElement("i");
+    newI1.setAttribute("class", "material-icons left");
+    newI1.textContent = "check";
+    newA1.appendChild(newI1);
+    newLi.appendChild(newA1);
+
+    const newA2 = document.createElement("a");
+    newA2.setAttribute("class", "waves-effect waves-light btn-small deleteBtn");
+
+    const newI2 = document.createElement("i");
+    newI2.setAttribute("class", "material-icons left");
+    newI2.textContent = "clear";
+    newA2.appendChild(newI2);
+    newLi.appendChild(newA2);
+
+    const ul = document.querySelector("div.bottom-app-container ul");
+    ul.appendChild(newLi);
+    counter++;
+    }
+    console.log(counter);
+});
+
+
+
+
+
